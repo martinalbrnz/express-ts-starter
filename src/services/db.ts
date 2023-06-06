3c1bd50c0d2f8347387c48b6cbf8b97291ba0a84
martinalbrnz <martinalbor96@gmail.com>
2023-06-06 02:44:27 -0300

    Add mongoose conn, commented

---
+++ src/services/db.ts
@@ -0,0 +1,12 @@
+/**
+ * Connection with mongodb and mongoose
+ * npm i mongoose mongodb @types/mongoose
+ */
+
+// import { connect } from 'mongoose';
+
+// export async function connectToDB(DATABASE_URL: string) {
+//   await connect(DATABASE_URL)
+//     .then(resp => console.log('Connected to DB'))
+//     .catch((err: Error) => console.log(err.message));
+// }
