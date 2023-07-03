06856c42177c83ff00028928915d0a82cfbb6b17
martinalbrnz <martinalbor96@gmail.com>
2023-07-03 20:23:50 -0300

    Add cors and cors types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express, { Request, Response } from 'express'
 import routes from './routes'
@@ -8,6 +9,7 @@ dotenv.config()
 const { PORT } = process.env
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.get('/', (req: Request, res: Response) => {
